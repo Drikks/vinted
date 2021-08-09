c9605189ea0edff64b9a1a574b85b39729dc922e
Bonnefoy <you@example.com>
2021-08-09 11:18:19 +0200

    vinted homepage

---
+++ src/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        Made at{" "}
+        <a
+          href="https://www.lereacteur.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Le Reacteur
+        </a>{" "}
+        by{" "}
+        <a
+          href="https://github.com/Drikks"
+          target="_blank"
+          rel=" noopener noreferrer"
+        >
+          Cédric
+        </a>
+        {""} - 2021{" "}
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
